9fcc027c95170dfc0897b1922a448049988e7e2c
dalbandea <albandeadavid@gmail.com>
2023-10-15 08:22:28 -0500

    allow to allocate beforehand register for reading

--- src/U1io.jl
+++ src/U1io.jl
@@ -7,10 +7,16 @@ function BDIO.BDIO_write!(fb::BDIO.BDIOstream, u1ws::U1)
     end
 end
 
-function BDIO.BDIO_read(fb::BDIO.BDIOstream, u1ws::U1)
-    reg = zeros(Float64,2)
+function BDIO.BDIO_read(fb::BDIO.BDIOstream, u1ws::U1, reg::Array{Float64, 1})
+    length(reg) != 2 && error("Register reg must be of length 2")
+
     for mu in 1:2, iy in 1:u1ws.params.iL[2], ix in 1:u1ws.params.iL[1]
         BDIO.BDIO_read(fb,reg)
         u1ws.U[ix,iy,mu] = complex(reg[1], reg[2])
     end
+end
+
+function BDIO.BDIO_read(fb::BDIO.BDIOstream, u1ws::U1)
+    reg = zeros(Float64,2)
+    BDIO_read(fb, u1ws, reg)
 end
