d91be1f6a01a48f54e1b82aa14fb77d94b78cbb6
David Albandea PROX15-2 <albandeadavid@gmail.com>
2024-03-05 09:58:57 +0100

    make scopes of test files local to each file

--- test/iotests.jl
+++ test/iotests.jl
@@ -1,41 +1,43 @@
 using LFTU1
 
-beta = 5.555
-lsize = 8
+let
+    beta = 5.555
+    lsize = 8
 
-model = U1Quenched(Float64,
-                   beta = beta,
-                   iL = (lsize, lsize),
-                   BC = OpenBC,
-                  )
+    model = U1Quenched(Float64,
+                       beta = beta,
+                       iL = (lsize, lsize),
+                       BC = OpenBC,
+                      )
 
-LFTU1.randomize!(model)
-fname = "U1iotest.bdio"
-LFTU1.save_cnfg(fname, model)
-fb, model2 = LFTU1.read_cnfg_info(fname, U1Quenched)
-LFTU1.read_next_cnfg(fb, model2)
-rm(fname, force=true)
+    LFTU1.randomize!(model)
+    fname = "U1iotest.bdio"
+    LFTU1.save_cnfg(fname, model)
+    fb, model2 = LFTU1.read_cnfg_info(fname, U1Quenched)
+    LFTU1.read_next_cnfg(fb, model2)
+    rm(fname, force=true)
 
-@testset "Quenched OBC" begin
-    @test model.params == model2.params
-    @test model.U == model2.U
-end
+    @testset "Quenched OBC" begin
+        @test model.params == model2.params
+        @test model.U == model2.U
+    end
 
 
-model = U1Quenched(Float64,
-                   beta = beta,
-                   iL = (lsize, lsize),
-                   BC = PeriodicBC,
-                  )
+    model = U1Quenched(Float64,
+                       beta = beta,
+                       iL = (lsize, lsize),
+                       BC = PeriodicBC,
+                      )
 
-LFTU1.randomize!(model)
-fname = "U1iotest.bdio"
-LFTU1.save_cnfg(fname, model)
-fb, model2 = LFTU1.read_cnfg_info(fname, U1Quenched)
-LFTU1.read_next_cnfg(fb, model2)
-rm(fname, force=true)
+    LFTU1.randomize!(model)
+    fname = "U1iotest.bdio"
+    LFTU1.save_cnfg(fname, model)
+    fb, model2 = LFTU1.read_cnfg_info(fname, U1Quenched)
+    LFTU1.read_next_cnfg(fb, model2)
+    rm(fname, force=true)
 
-@testset "Quenched PBC" begin
-    @test model.params == model2.params
-    @test model.U == model2.U
+    @testset "Quenched PBC" begin
+        @test model.params == model2.params
+        @test model.U == model2.U
+    end
 end
--- test/u1nf2tests.jl
+++ test/u1nf2tests.jl
@@ -1,36 +1,37 @@
 using LinearAlgebra
 
+let
+    # U1 Theory parameters
+    lsize = 8
+    beta = 5.0
+    mass = 0.6
 
-# U1 Theory parameters
-lsize = 8
-beta = 5.0
-mass = 0.6
+    model = LFTU1.U1Nf2(Float64, iL = (lsize, lsize), beta = beta, am0 = mass, BC = PeriodicBC)
 
-model = LFTU1.U1Nf2(Float64, iL = (lsize, lsize), beta = beta, am0 = mass, BC = PeriodicBC)
+    alg = HMC(
+              integrator = Leapfrog(1.0, 10),
+             )
 
-alg = HMC(
-          integrator = Leapfrog(1.0, 10),
-         )
+    hmcws = LFTU1.sampler(model, alg)
 
-hmcws = LFTU1.sampler(model, alg)
+    LFTU1.randomize!(model)
+    LFTU1.generate_pseudofermions!(model, hmcws)
 
-LFTU1.randomize!(model)
-LFTU1.generate_pseudofermions!(model, hmcws)
+    @testset "Pseudofermion generation" begin
+        xi = similar(hmcws.X)
+        invert!(xi, gamm5Dw_sqr_msq!, hmcws.F, model.sws, model)
+        @test isapprox(dot(xi, hmcws.F), dot(hmcws.X, hmcws.X))
+    end
 
-@testset "Pseudofermion generation" begin
-    xi = similar(hmcws.X)
-    invert!(xi, gamm5Dw_sqr_msq!, hmcws.F, model.sws, model)
-    @test isapprox(dot(xi, hmcws.F), dot(hmcws.X, hmcws.X))
-end
+    @testset "HMC reversibility" begin
+        model_bckp = deepcopy(model)
+        LFTSampling.reversibility!(model, hmcws)
+        ΔU = model.U .- model_bckp.U
+        @test isapprox(zero(model.PRC), mapreduce(x -> abs2(x), +, ΔU), atol = 1e-15)
+    end
 
-@testset "HMC reversibility" begin
-    model_bckp = deepcopy(model)
-    LFTSampling.reversibility!(model, hmcws)
-    ΔU = model.U .- model_bckp.U
-    @test isapprox(zero(model.PRC), mapreduce(x -> abs2(x), +, ΔU), atol = 1e-15)
-end
-
-@testset verbose = true "HMC force" begin
-    ΔF = LFTSampling.force_test(model, hmcws, 1e-7)
-    @test isapprox(zero(model.PRC), ΔF, atol = 1e-5)
+    @testset verbose = true "HMC force" begin
+        ΔF = LFTSampling.force_test(model, hmcws, 1e-7)
+        @test isapprox(zero(model.PRC), ΔF, atol = 1e-5)
+    end
 end
