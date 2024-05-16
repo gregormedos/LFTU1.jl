95ecf4677c4c1b2044ff92bc90b6245d78a6da35
David Albandea PROX15-2 <albandeadavid@gmail.com>
2024-05-16 16:50:18 +0200

    remove functions that have been upgraded to the main code

--- main/Nf/nf-compute-corrs.jl
+++ main/Nf/nf-compute-corrs.jl
@@ -18,21 +18,25 @@ function parse_commandline(args)
         help = "lattice size"
         required = true
         arg_type = Int
+
         "--start"
         help = "start from configuration"
         required = false
         arg_type = Int
         default = 1
+
         "--nconf"
         help = "number of configurations to analyze; 0 means until the end"
         required = false
         arg_type = Int
         default = 0
+
         "--nsrc"
         help = "number of sources"
         required = false
         arg_type = Int
         default = 2
+
         "--ens"
         help = "path to ensemble with configurations"
         required = true
@@ -71,115 +75,6 @@ finish = start + ncfgs - 1
 
 fb, model = read_cnfg_info(cfile, U1Nf)
 
-struct U1Correlator <: LFTU1.AbstractU1Correlator
-    name::String
-    ID::String
-    filepath::String
-    R
-    S
-    S0
-    result::Vector{Float64} # correlator
-    history::Vector{Vector{Float64}}
-    function U1Correlator(u1ws::LFTU1.U1; wdir::String = "./trash/", 
-                               name::String = "U(1) correlator", 
-                               ID::String = "corr_pion", 
-                               mesdir::String = "measurements/", 
-                               extension::String = ".txt")
-        dt = Dates.now()
-        wdir_sufix = "_D"*Dates.format(dt, "yyyy-mm-dd-HH-MM-SS.ss")
-        lp = u1ws.params
-        filepath = joinpath(wdir, mesdir, ID*wdir_sufix*extension)
-        R1 = LFTU1.to_device(u1ws.device, zeros(complex(Float64), lp.iL..., 2))
-        R2 = copy(R1)
-        S = copy(R1)
-        S0 = copy(R1)
-        C = zeros(Float64, lp.iL[1])
-        history = []
-        mkpath(dirname(filepath))
-        return new(name, ID, filepath, [R1, R2], S, S0, C, history)
-    end
-end
-
-
-"""
-Generate complex random normal source at time slice t0 storing it to corrws.S0, and solve γ₅D corrws.R[ifl] = corrws.S0 for each flavor ifl.
-"""
-function random_source(t0, corrws, u1ws)
-    S0 = corrws.S0
-    S = corrws.S
-    R = corrws.R
-    lp = u1ws.params
-    S0 .= zero(ComplexF64)
-    S0[:,t0,:] .= randn(ComplexF64, lp.iL[1],2)
-    for ifl in 1:2
-        ## Solve g5D R = S0 for S for Flavor ifl
-        iter = LFTU1.invert!(S, LFTU1.gamm5Dw_sqr_msq_am0!(model.params.am0[ifl]), S0, model.sws, model)
-        gamm5Dw!(R[ifl], S, model.params.am0[ifl], model)
-    end
-    return nothing
-end
-
-"""
-Computes dot(corrws.R[ifl], corrws.R[jfl]), i.e. (D_ifl⁻¹ η, D_jfl⁻¹ η), at time slice t.
-"""
-function connected_correlator(corrws::U1Correlator, t, u1ws, ifl, jfl)
-    lp = u1ws.params
-
-    Ct = zero(ComplexF64)
-    a = zero(ComplexF64)
-    b = zero(ComplexF64)
-
-    # NOTE: this should be ultraslow. It may be better to put R1 and R2 into the
-    # CPU prior to calling this function. For GPU, the best one can do is to
-    # reduce columns of the GPU array.
-    LFTU1.allowscalar(u1ws.device)
-    for x in 1:lp.iL[1]
-        a = corrws.R[jfl][x,t,:]
-        b = corrws.R[ifl][x,t,:]
-        Ct += abs(dot(b,a)) / lp.iL[1]
-    end
-    LFTU1.disallowscalar(u1ws.device)
-
-    return Ct
-end
-
-"""
-Computes dot(corrws.S0, corrws.R[ifl]), i.e. (η^†, D_ifl⁻¹ η), at time slice t.
-"""
-function disconnected_correlator(corrws, t, u1ws, ifl)
-    lp = u1ws.params
-
-    Ct = zero(ComplexF64)
-    a = zero(ComplexF64)
-    b = zero(ComplexF64)
-
-    # NOTE: this should be ultraslow. It may be better to put R1 and R2 into the
-    # CPU prior to calling this function. For GPU, the best one can do is to
-    # reduce columns of the GPU array.
-    LFTU1.allowscalar(u1ws.device)
-    for x in 1:lp.iL[1]
-        a = corrws.S0[x,t,:]
-        b = corrws.R[ifl][x,t,:]
-        Ct += real(dot(b,a)) / sqrt(lp.iL[1])
-    end
-    LFTU1.disallowscalar(u1ws.device)
-
-    return Ct
-end
-
-function connected_correlator(corrws::U1Correlator, u1ws, ifl, jfl)
-    lp = u1ws.params
-    for t in 1:lp.iL[1]
-        corrws.result[t] = connected_correlator(corrws, t, u1ws, ifl, jfl) |> real
-    end
-end
-
-function disconnected_correlator(corrws::U1Correlator, u1ws, ifl)
-    lp = u1ws.params
-    for t in 1:lp.iL[1]
-        corrws.result[t] = disconnected_correlator(corrws, t, u1ws, ifl) |> real
-    end
-end
 
 function reset_data(data)
     data.P .= 0.0
