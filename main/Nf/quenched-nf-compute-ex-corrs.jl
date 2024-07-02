4c526badb7ef64b43e8764c935ed3bbbd10c0de5
gregormedos <medos.gregor@gmail.com>
2024-07-02 16:19:11 +0200

    feat!: add calculations of traces for exact 4-point correlators

--- main/Nf/quenched-nf-compute-ex-corrs.jl
+++ main/Nf/quenched-nf-compute-ex-corrs.jl
@@ -72,9 +72,22 @@ finish = start + ncfgs - 1
 fb, model = read_cnfg_info(cfile, U1Quenched)
 
 data = (
+    # for building 2-point correlators
     P = zeros(Float64, NFL, NFL, N0),
     disc = zeros(Float64, NFL, NFL, N0),
-    Delta = zeros(Float64, NFL, N0)
+    Delta = zeros(Float64, NFL, N0),
+    # for bulding 4-point correlators
+    P4 = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_1DeltaInitial = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_1DeltaFinal = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_2PDeltas = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_2PDeltasSpatial = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_2Deltas = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_2DeltasSpatial = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    disc4_4Deltas = zeros(Float64, NFL, NFL, NFL, NFL, N0),
+    PDelta = zeros(Float64, NFL, NFL, N0),
+    P3Initial = zeros(Float64, NFL, NFL, NFL, N0),
+    P3Final = zeros(Float64, NFL, NFL, NFL, N0)
 )
 Data = typeof(data)
 
@@ -82,29 +95,70 @@ function reset_data(data::Data)
     data.P .= 0.0
     data.disc .= 0.0
     data.Delta .= 0.0
+    data.P4 .= 0.0
+    data.disc4_1DeltaInitial .= 0.0
+    data.disc4_1DeltaFinal .= 0.0
+    data.disc4_2PDeltas .= 0.0
+    data.disc4_2PDeltasSpatial .= 0.0
+    data.disc4_2Deltas .= 0.0
+    data.disc4_2DeltasSpatial .= 0.0
+    data.disc4_4Deltas .= 0.0
+    data.PDelta .= 0.0
+    data.P3Initial .= 0.0
+    data.P3Final .= 0.0
     return nothing
 end
 
 """
 - Compute connected traces and save them into data.P[ifl, jfl, t]
 - Compute disconnected traces separately and save them to data.Delta[ifl, t]
+- Compute spatially connected but temporally disconnected traces separately and save them into data.PDelta[ifl, jfl, t]
+- Compute initially connected 3-point traces separately and save them into data.P3Initial[ifl1, ifl2, ifl3, t]
+- Compute finally connected 3-point traces separately and save them into data.P3Final[ifl1, ifl2, ifl3, t]
+- Compute connected 4-point traces and save them into data.P4[ifl1, ifl2, ifl3, ifl4, t]
 """ 
 function correlators(data::Data, corrws::U1exCorrelator, u1ws)
     reset_data(data)
-    for ifl in 1:2
+    for ifl in 1:NFL
         ex_disconnected_correlator(corrws, u1ws, ifl)
         data.Delta[ifl,:] .+= corrws.result
-        for jfl in 1:2
+        for jfl in 1:NFL
             for it in 1:N0
                 ex_connected_correlator(corrws, u1ws, it, ifl, jfl)
                 for t in 1:N0
                     tt=((t-it+N0)%N0+1);
                     data.P[ifl,jfl,tt] += corrws.result[t] / N0
                 end
+            ex_spatially_connected_correlator(corrws, u1ws, ifl, jfl)
+            data.PDelta[ifl,jfl,:] .+= corrws.result
+            end
+            for ifl3 in 1:NFL
+                for it in 1:N0
+                    ex_3point_initially_connected_correlator(corrws, u1ws, it, ifl, jfl, ifl3)
+                    for t in 1:N0
+                        tt=((t-it+N0)%N0+1);
+                        data.P3Initial[ifl,jfl,ifl3,tt] += corrws.result[t] / N0
+                    end
+                    ex_3point_finally_connected_correlator(corrws, u1ws, it, ifl, jfl, ifl3)
+                    for t in 1:N0
+                        tt=((t-it+N0)%N0+1);
+                        data.P3Final[ifl,jfl,ifl3,tt] += corrws.result[t] / N0
+                    end
+                end
+                for ifl4 in 1:NFL
+                    for it in 1:N0
+                        ex_4point_connected_correlator(corrws, u1ws, it, ifl, jfl, ifl3, ifl4)
+                        for t in 1:N0
+                            tt=((t-it+N0)%N0+1);
+                            data.P4[ifl,jfl,ifl3,ifl4,tt] += corrws.result[t] / N0
+                        end
+                    end
+                end
             end
         end
     end
     compute_disconnected!(data)
+    compute_disconnected4!(data)
     return nothing
 end
 
@@ -113,7 +167,7 @@ Compute all combinations of disconnected traces and save them to data.disc[ifl, 
 """
 function compute_disconnected!(data::Data)
     data.disc .= 0.0
-    for ifl in 1:2, jfl in ifl:2
+    for ifl in 1:NFL, jfl in ifl:NFL
         for t in 1:N0, tt in 1:N0
             data.disc[ifl, jfl, t] += data.Delta[ifl, tt] * data.Delta[jfl, (tt+t-1-1)%N0+1] / N0
         end
@@ -121,11 +175,26 @@ function compute_disconnected!(data::Data)
     return nothing
 end
 
+"""
+Compute all combinations of disconnected traces and save them to:
+- data.disc4_1DeltaInitial[ifl, ifl2, ifl3, ifl4, t]
+- data.disc4_1DeltaFinal[ifl, ifl2, ifl3, ifl4, t]
+- data.disc4_2PDeltas[ifl, ifl2, ifl3, ifl4, t]
+- data.disc4_2PDeltasSpatial[ifl, ifl2, ifl3, ifl4, t]
+- data.disc4_2Deltas[ifl, ifl2, ifl3, ifl4, t]
+- data.disc4_2DeltasSpatial[ifl, ifl2, ifl3, ifl4, t]
+- data.disc4_4Deltas[ifl, ifl2, ifl3, ifl4, t]
+"""
+function compute_disconnected4!(data::Data)
+    # TODO: 7 types of combinations
+    return nothing
+end
+
 function save_data(data::Data, dirpath)
-    for ifl in 1:2
+    for ifl in 1:NFL
         deltafile = joinpath(dirpath, "measurements/exdelta-$(ifl)_confs$start-$finish.txt")
         write_vector(data.Delta[ifl, :],deltafile)
-        for jfl in ifl:2
+        for jfl in ifl:NFL
             connfile = joinpath(dirpath,"measurements/exconn-$ifl$(jfl)_confs$start-$finish.txt")
             discfile = joinpath(dirpath, "measurements/exdisc-$ifl$(jfl)_confs$start-$finish.txt")
             write_vector(data.P[ifl, jfl, :],connfile)
