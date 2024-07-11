efdccfa4114bc649f2c399be9a537a394c71e524
gregormedos <medos.gregor@gmail.com>
2024-07-11 12:31:57 +0200

    feat!: update quenched-main for cluster

--- main/quenched/quenched-main.jl
+++ main/quenched/quenched-main.jl
@@ -1,37 +1,126 @@
+# Quantum Rotor
 using Revise
 import Pkg
 Pkg.activate(".")
+using TOML
+
+length(ARGS) == 1 || error("Only one argument is expected! (Path to input file)")
+isfile(ARGS[1]) || error("Path provided is not a file")
+
+if length(ARGS) == 1
+    infile = ARGS[1]
+else
+    infile = "main/quenched/infile.in"
+end
+pdata = TOML.parsefile(infile)
+device = pdata["Model params"]["device"]
+if device == "CUDA"
+    import CUDA
+    device = CUDA.device()
+elseif device == "CPU"
+    import KernelAbstractions
+    device = KernelAbstractions.CPU()
+else
+    error("Only acceptable devices are CUDA or CPU")
+end
+
 using LFTSampling
 using LFTU1
-using ProgressBars
+using Dates
+using Logging
 
-# Params
-beta = 5.0 # value of the coupling of the theory
-lsize = 20 # lattice size
-BC = PeriodicBC # Boundary Condition
-tau = 1.0 # trajectory length of Moledular Dynamics evolution
-nsteps = 10 # number of steps of integration, tune to have 80% or more
-
-# Store configurations
-nsamples = 5
-filename = "test"
-
-# Create model workspace U1 quenched theory
-model = U1Quenched(
-    Float64,
-    beta = beta,
-    iL = (lsize, lsize),
-    BC = BC,
-)
-
-# Run HMC sampling
-LFTU1.coldstart!(model)
-
-# Create sampler workspace
-samplerws = LFTSampling.sampler(model, HMC(integrator = Leapfrog(tau, nsteps)))
-
-# Sample 50,000 configurations and save them to "test.bdio"
-for _ in ProgressBar(1:nsamples)
-    sample!(model, samplerws)
-    save_cnfg(joinpath(@__DIR__, filename*".bdio"), model)
+function create_simulation_directory(wdir::String, u1ws::U1Quenched)
+    dt = Dates.now()
+    wdir_sufix = "_D"*Dates.format(dt, "yyyy-mm-dd-HH-MM-SS.ss")
+    fname = "quenchedsim-b$(u1ws.params.beta)-L$(model.params.iL[1])-m$(u1ws.params.am0)"*wdir_sufix
+    fdir = joinpath(wdir, fname)
+    configfile = joinpath(fdir, fname*".bdio")
+    mkpath(fdir)
+    cp(infile, joinpath(fdir,splitpath(infile)[end]))
+    return configfile
 end
+
+# Read model parameters
+
+beta = pdata["Model params"]["beta"]
+mass = pdata["Model params"]["mass"]
+lsize = pdata["Model params"]["L"]
+BC = eval(Meta.parse(pdata["Model params"]["BC"]))
+
+# Read HMC parameters
+
+tau = pdata["HMC params"]["tau"]
+nsteps = pdata["HMC params"]["nsteps"]
+ntherm = pdata["HMC params"]["ntherm"]
+ntraj = pdata["HMC params"]["ntraj"]
+discard = pdata["HMC params"]["discard"]
+integrator = eval(Meta.parse(pdata["HMC params"]["integrator"]))
+
+# Working directory
+
+wdir = pdata["Working directory"]["wdir"]
+cntinue = pdata["Working directory"]["continue"]
+cntfile = pdata["Working directory"]["cntfile"]
+
+
+model = U1Quenched(
+              Float64, 
+              beta = beta, 
+              iL = (lsize, lsize), 
+              BC = PeriodicBC,
+              device = device,
+             )
+
+randomize!(model)
+smplr = HMC(integrator = integrator(tau, nsteps))
+samplerws = LFTSampling.sampler(model, smplr)
+
+if cntinue == true
+    @info "Reading from old simulation"
+    configfile = cntfile
+    ncfgs = LFTSampling.count_configs(configfile)
+    fb, model = read_cnfg_info(configfile, U1Quenched)
+    LFTSampling.read_cnfg_n(fb, ncfgs, model)
+    close(fb)
+else
+    @info "Creating simulation directory"
+    ncfgs = 0
+    configfile = create_simulation_directory(wdir, model)
+end
+
+logio = open(dirname(configfile)*"/log.txt", "a+")
+logger = SimpleLogger(logio)
+global_logger(logger)
+
+@info "U(1) NF=2 SIMULATION" model.params smplr
+
+if cntinue == true
+    @info "Skipping thermalization"
+else
+    @info "Starting thermalization"
+    for i in 1:ntherm
+        @info "THERM STEP $i"
+        @time sample!(model, samplerws)
+        flush(logio)
+    end
+end
+
+if cntinue == true
+    @info "Restarting simulation from trajectory $ncfgs"
+else
+    @info "Starting simulation"
+end
+
+@time for i in (ncfgs+1):(ncfgs+ntraj)
+    @info "TRAJECTORY $i"
+    for j in 1:discard
+        @time sample!(model, samplerws)
+    end
+    @time sample!(model, samplerws)
+    save_cnfg(configfile, model)
+    flush(logio)
+end
+
+@info "Simulation finished succesfully"
+flush(logio)
+close(logio)
